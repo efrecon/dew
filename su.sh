4e6d0f9ab43e313fa84df243fe4d51614e981c3e
Emmanuel Frécon <efrecon@gmail.com>
2021-04-26 12:18:15 +0200

    Fix for ubuntu

--- su.sh
+++ su.sh
@@ -17,6 +17,91 @@ DOCKER_GROUP=${DOCKER_GROUP:-docker}
 # to the group being created.
 DOCKER_SOCKET=${DOCKER_SOCKET:-/var/run/docker.sock}
 
+# When set, this will print some extra logging around user/group creation and
+# shell detection. The variable will be set when verbosity is set to trace.
+DEW_DEBUG=${DEW_DEBUG:-0}
+
+# Log the text passed as a paramter whenever DEW_DEBUG is 1
+log() {
+  if [ "$DEW_DEBUG" = "1" ]; then
+    printf %s\\n "$1" >/dev/stderr
+  fi
+}
+
+# Return the name of the Linux distribution this is running on, in lowercase.
+distro() {
+  if [ -r "/etc/os-release" ]; then
+    # shellcheck disable=SC1091 # /etc/os-release is standardised
+    lsb_dist=$(. /etc/os-release && printf %s\\n "$ID")
+    printf %s\\n "$lsb_dist" | tr '[:upper:]' '[:lower:]'
+  fi
+}
+
+# Create a user group called $USER with the identifier $DEW_GID
+create_group() {
+  log "Adding group $USER with id $DEW_GID to /etc/group"
+  case "$(distro)" in
+    ubuntu* | debian*)
+      addgroup -q --gid "$DEW_GID" "$USER";;
+    alpine*)
+      addgroup -g "$DEW_GID" "$USER";;
+    fedora)
+      groupadd -g "$DEW_GID" "$USER";;
+    *)
+      # Go old style, just add an entry to the /etc/group file
+      printf "%s:x:%d:\n" "$USER" "$DEW_GID" >> /etc/group;;
+  esac
+}
+
+# Create a user $USER, belonging to the group of the same name (created by
+# function above), with identifier $DEW_UID and shell $SHELL, as detected at the
+# when this script starts
+create_user() {
+  log "Adding user $USER with id $DEW_UID to /etc/passwd. Shell: $SHELL"
+  case "$(distro)" in
+    ubuntu* | debian*)
+      adduser \
+        -q \
+        --home "$HOME" \
+        --shell "$SHELL" \
+        --gid "$DEW_GID" \
+        --disabled-password \
+        --uid "$DEW_UID" \
+        --gecos "" \
+        "$USER";;
+    alpine*)
+      # Alpine uses the name of the group, which is the same as the user in our
+      # case.
+      adduser \
+        -h "$HOME" \
+        -s "$SHELL" \
+        -G "$USER" \
+        -D \
+        -u "$DEW_UID" \
+        -g "" \
+        "$USER";;
+    fedora)
+      useradd \
+        --home-dir "$HOME" \
+        --shell "$SHELL" \
+        --gid "$DEW_GID" \
+        --password "" \
+        --uid "$DEW_UID" \
+        --comment "" \
+        "$USER";;
+    *)
+      # Go old style, just add an entry to the /etc/passwd file
+      printf "%s:x:%d:%d::%s:%s\\n" \
+            "$USER" \
+            "$DEW_UID" \
+            "$DEW_GID" \
+            "$HOME" \
+            "$SHELL" >> /etc/passwd;;
+  esac
+
+}
+
+
 # Decide upon a good plausible shell to run, the order might be questionable,
 # but this covers a large set of distributions and shells.
 SHELL=
@@ -26,14 +111,17 @@ for s in bash ash sh; do
     break
   fi
 done
+log "Default shell detected as $SHELL"
 
 if [ "${USER:-}" = "root" ] || [ -z "${USER:-}" ]; then
   # When running as root or an unknown user, we just replace ourselves with
   # either the default shell that we guessed existed in the container, or with
   # the one forced in from the outside by dew.
   if [ -n "${DEW_SHELL:-}" ]; then
+    log "Replacing ourselves with $DEW_SHELL"
     exec "$DEW_SHELL"
   else
+    log "Replacing ourselves with $SHELL"
     exec "$SHELL"
   fi
 else
@@ -43,6 +131,7 @@ else
 
   # Create a home for the user and make sure it is accessible for RW
   if [ -n "$HOME" ]; then
+    log "Creating home directory $HOME, owned by ${DEW_UID:-0}:${DEW_GID:-0}"
     mkdir -p "$HOME"
     chown "${DEW_UID:-0}:${DEW_GID:-0}" "${HOME}"
     chmod ug+rwx "$HOME"
@@ -55,7 +144,7 @@ else
     if [ -f "/etc/group" ]; then
       # Create the group if it does not already exist at the same GID
       if ! cut -d: -f3 /etc/group | grep -q "$DEW_GID"; then
-        printf "%s:x:%d:\n" "$USER" "$DEW_GID" >> /etc/group
+        create_group
       fi
     fi
 
@@ -63,12 +152,7 @@ else
     # shell to be the one discovered at the beginning of this script.
     if [ -f "/etc/passwd" ] && [ -n "${DEW_UID:-}" ]; then
       if ! cut -d: -f1 /etc/passwd | grep -q "$USER"; then
-        printf "%s:x:%d:%d::%s:%s\\n" \
-              "$USER" \
-              "$DEW_UID" \
-              "$DEW_GID" \
-              "$HOME" \
-              "$SHELL" >> /etc/passwd
+        create_user
       fi
     fi
   fi
@@ -79,6 +163,7 @@ else
       ! cut -d: -f1 /etc/group | grep -q "^${DOCKER_GROUP}:" && \
       [ -S "$DOCKER_SOCKET" ]; then
     dgid=$(stat -c '%g' "$DOCKER_SOCKET")
+    log "Making user $USER member of the group $DOCKER_GROUP with id $dgid to /etc/passwd"
     printf "${DOCKER_GROUP}:x:%d:%s\n" "$dgid" "$USER" >> /etc/group
   fi
 
@@ -90,9 +175,14 @@ else
   # single binary in them.
   if command -v "su" >/dev/null 2>&1; then
     if [ -z "${DEW_SHELL:-}" ]; then
+      log "Becoming $USER, running $SHELL as set in /etc/passwd"
       exec su "$USER"
     else
+      log "Becoming $USER, running $DEW_SHELL"
       exec su -s "$(command -v "$DEW_SHELL")" "$USER"
     fi
+  else
+    log "Cannot find su"
+    exit 1
   fi
 fi
