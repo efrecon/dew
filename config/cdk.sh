ef386a9fb1cef52979f1a2d5433ef804c7f5ad19
Emmanuel Frécon <emmanuel.frecon@gpsgate.com>
2022-05-12 13:16:38 +0200

    Install depends
    This installs dependencies for cdk init

--- config/cdk.sh
+++ config/cdk.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-npm install -g typescript && npm install -g aws-cdk
+# Install git as it is used for `cdk init`
+if command -v apk >/dev/null 2>&1; then
+    apk add --no-cache git
+elif command -v apt-get >/dev/null 2>&1; then
+    apt-get update -y && apt-get install -y git
+fi
+
+# Upgrade npm to the latest, then install typescript and cdk globally, so they
+# are available at the prompt. This doesn't pin versions, which might be against
+# some dev practices.
+npm update -g npm && npm install -g typescript aws-cdk
