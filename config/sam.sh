f70fdb15d7195cbd165820de4a1ad1fe932b20da
Emmanuel Frécon <emmanuel.frecon@gpsgate.com>
2023-03-07 09:01:48 +0100

    Add AWS Serverless Application Model CLI

--- config/sam.sh
+++ config/sam.sh
@@ -7,3 +7,12 @@ if [ ! -x "/usr/local/bin/session-manager-plugin" ]; then
   yum install -y /tmp/session-manager-plugin.rpm
   session-manager-plugin
 fi
+
+if ! command -v sam >/dev/null 2>&1; then
+  curl -sSL "https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip" \
+    -o /tmp/aws-sam-cli-linux.zip
+  yum install -y unzip
+  unzip /tmp/aws-sam-cli-linux.zip -d /tmp/aws-sam-cli-installer
+  /tmp/aws-sam-cli-installer/install
+  sam --version
+fi
