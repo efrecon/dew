2c65592459fdacef8fc3d4583f69cd6c347fc809
Emmanuel Frecon <9443924+efrecon@users.noreply.github.com>
2024-09-30 23:45:21 +0200

    Improve podman compatibility (#47)

--- libexec/utils.sh
+++ libexec/utils.sh
@@ -10,3 +10,38 @@ digest() {
 tac() {
   awk '{a[i++]=$0} END {for (j=i-1; j>=0;) print a[j--] }' "$@"
 }
+
+# Generate a bindmount option, arrange for Z to be passed as an option when
+# using podman.
+bindmount() {
+  stack_let src
+  stack_let dst
+  stack_let opt
+
+  src=$1
+  if [ -z "${2:-}" ]; then
+    dst=$1
+  else
+    dst=$2
+  fi
+
+  if [ "$DEW_RUNTIME" = "podman" ]; then
+    if [ -n "${3:-}" ]; then
+      opt="$3,Z"
+    else
+      opt=Z
+    fi
+  elif [ -n "${3:-}" ]; then
+    opt="$3"
+  fi
+
+  if [ -n "${opt:-}" ]; then
+    printf '%s:%s:%s\n' "$src" "$dst" "$opt"
+  else
+    printf '%s:%s\n' "$src" "$dst"
+  fi
+
+  stack_unlet src
+  stack_unlet dst
+  stack_unlet opt
+}
